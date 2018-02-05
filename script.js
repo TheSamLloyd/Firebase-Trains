0e5320e72d3f7c31652b9f768ce68f1059d3ffbf
TheSamLloyd <samlloydj@gmail.com>
2018-02-05 01:28:51 -0500

    all functionality finished

--- script.js
+++ script.js
@@ -20,14 +20,21 @@ $("#submit").on("click",function(){
   };
   firebase.database().ref().push(newTrain)
 })
-firebase.database().ref().on("value",function(snapshot){
-  for (i in snapshot.val()){
-    console.log(snapshot.val()[i])
-    newrow = $("<tr>")
-    .append("<td>"+snapshot.val()[i]["name"])
-    .append("<td>"+snapshot.val()[i]["destination"])
-    .append("<td>"+snapshot.val()[i]["frequency"])
-    .append("<td>"+snapshot.val()[i]["firstTime"]);
-    $("#trains-list").append(newrow)
-  };
+firebase.database().ref().on("child_added",function(snapshot){
+  console.log(snapshot.val())
+  newrow = $("<tr>")
+  .append("<td>"+snapshot.val()["name"])
+  .append("<td>"+snapshot.val()["destination"])
+  .append("<td>"+moment.duration({minutes : snapshot.val()["frequency"]}).humanize())
+  .append("<td>"+nextTime(snapshot.val()["frequency"],snapshot.val()["firstTime"]).format("hh:mm a"))
+  .append("<td>"+minutesOut(snapshot.val()["frequency"],snapshot.val()["firstTime"]));
+  $("#trains-list").append(newrow)
 })
+function nextTime(frequency,firstTime){
+  frequency=parseInt(frequency);
+  return moment(firstTime,"HH:mm").add(frequency*Math.ceil((moment().unix()-moment(firstTime, "HH:mm").unix())/(60*frequency)),"minutes")
+}
+function minutesOut(frequency,firstTime){
+  return nextTime(frequency,firstTime).fromNow(false)
+}
+nextTime("16:30","44")
